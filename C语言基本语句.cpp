bd2d397a4fb656eafa48470adb0378e5bbe4dba7
42239599 <88647659+42239599@users.noreply.github.com>
2021-12-01 21:01:22 +0800

    Add files via upload

---
+++ C语言基本语句.cpp
@@ -0,0 +1,109 @@
+//固定格式
+/*
+#include <stdui.h>
+int main()
+{
+
+	return 0;
+}
+
+*/ 
+
+
+//printf 语句
+/*
+#include <stdio.h>
+int main()
+{
+	printf("我是");
+	printf("天下");
+	printf("第一");
+	printf("帅猴");
+	return 0; 
+	
+}//我是天下第一帅猴 
+
+*/ 
+
+
+////int float double char 型数据
+/*
+	int a = ;
+	float a = 3.14;
+	double a = 3.14;
+	char = 'A';
+	int a=3,b,c=5;
+
+*/ 
+//#include <stdio.h>
+//int main()
+//{
+//	int a=5,b,c,d,e,f;
+//	b=a+2;
+//	c=b-a;
+//	d=a*c;
+//	e=a/b;
+//	f=a%d;
+//	printf("%d %d %d",b,c,d);
+//	
+//	return 0;
+//}
+//x*=y+8----->x=x*(y+8); 
+
+//#include <stdio.h>
+//int main()
+//{
+//	double a,b,c,s;
+//	a=3.67;
+//	b=5.43;
+//	c=6.21;
+//	s=(a+b+c)/3;
+//	printf("s is :%f",s);
+//	return 0;
+//}
+
+/*
+用 scanf语句输入 int,float,double,char 
+*/
+//#include <stdio.h>
+//int main()
+//{
+//	char a,b;
+//	scanf("%c",&a);
+//	b = a+32;
+//	printf("%c 的小写字母是%c\n",a,b);
+//	
+//	return 0;
+//}
+
+
+/*
+	putchar()语句和 getchar（）语句 
+*/
+//#include <stdio.h>
+//int main()
+//{
+//	char a,b,c;
+//	a=getchar();
+//	b=getchar();
+//	c=getchar();
+//	a=a+32;
+//	b=b+32;
+//	c=c+32;
+//	putchar(a);
+//	putchar(b);
+//	putchar(c);
+//	putchar('\n');
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+ 
