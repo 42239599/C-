a9f8d7e79bc1f441f538adc25de8cd06dc3daa84
42239599 <88647659+42239599@users.noreply.github.com>
2021-12-08 21:17:54 +0800

    Add files via upload

---
+++ 书本.cpp
@@ -0,0 +1,50 @@
+/////求两数中的较大数
+//#include <stdio.h>
+//int main()
+//{
+//	int max(int x,int y);
+//	int a,b,c;
+//	printf("请输入两个数字：\n");
+//	scanf("%d %d",&a,&b);
+//	c=max(a,b);
+//	printf("较大数为:%d\n",c);
+//	return 0;
+// } 
+//int max(int x,int y)
+//{
+//	return(x>y?x:y);
+//}
+
+
+///有人用温度计测量出用华氏法表示的温度，今要求把它转换为以摄氏法表示的温度
+///c=5/9(f-32) 
+//#include <stdio.h>
+//int main()
+//{
+//	float c,f;
+//	printf("请输入温度：\n");
+//	scanf("%f",&f);
+//	c=5.0/9.0*(f-32.0);
+//	printf("温度是：%5.2f\n",c); 
+//	
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	char c1,c2;
+//	c1='A';
+//	c2=c1+32;
+//	printf("%c\n",c2);
+//	printf("%d\n",c2);
+//	return 0;
+//}
+
+
+
+
+
+
+
+
