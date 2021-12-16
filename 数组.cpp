1b3d576aef158c7fac36e63dfdd2a3ca48c2e48b
42239599 <88647659+42239599@users.noreply.github.com>
2021-12-16 21:23:25 +0800

    Add files via upload

--- 数组.cpp
+++ 数组.cpp
@@ -1,93 +1,16 @@
-////数组
-//定义一维数组
-////输入十个地区的面积，对他们由小到大排序并输出排序后的结果
 //#include <stdio.h>
 //int main()
 //{
-//	int a[10];
-//	int i,j,t;
-//	printf("请输入十个面积:\n");
-//	for(i=0;i<10;i++)
-//	{
-//		scanf("%d",&a[i]);
-//	}
-//	for(i=0;i<9;i++)
-//	{
-//		for(j=i+1;j<10;j++)
-//		{
-//			if(a[i]>a[j])
-//			{
-//				t = a[i];
-//				a[i] =a[j];
-//				a[j] =t;
-//			}
-//		}
-//	}	
-//	printf("排序后依次是：\n");
-//	for(i=0;i<10;i++)	
-//	printf("%d\t",a[i]);
-//	printf("\n");
-//	return 0;
-// } 
-
-//对十个数组元素依次赋值为0,1,2,3,4,5,6,7,8,9,并按倒序输出
-//#include <stdio.h>
-//int main()
-//{
-//	int a[10] = {0,1,2,3,4,5,6,7,8,9};
-//	int i=9;
-//	for(i=9;i>=0;i--)
-//	{
-//		printf("%d\t",a[i]);
-//	}
-//	return 0;
-// } 
-
-//#include <stdio.h>
-//int main()
-//{
-//	int a[10];
-//	int i,j,t;
-//	printf("请输入十个数：\n");
-//	for(i=0;i<10;i++)
-//	{
-//		scanf("%d",&a[i]);
-//	}
-//	for(j=0;j<9;j++)//第一个数 排9次 
-//	{
-//		for(i=0;i<9-j;i++)
-//		{
-//			if(a[i]>a[i+1])
-//			{
-//				t = a[i];
-//				a[i] = a[i+1];
-//				a[i+1] = t;
-//			}
-//		}
-//	}
-//	printf("排序后为：\n");
-//	for(i=0;i<10;i++)
-//	printf("%d\t",a[i]);
-//	printf("\n");
-//	return 0;
-//}
-
-//123 456 789 147 258 369 963 852 741 888
-//选择排序 
-//#include <stdio.h>
-//int main()
-//{
-//	int a[10];
-//	int i,j,t;
+//	int a[10],i,j,t;
 //	printf("请输入10个数字：\n");
-//	for(i=0;i<10;i++)
+//	for(i=0;i<=9;i++)
 //	{
 //		scanf("%d",&a[i]);
-//	 } 
+//	}
 //	
-//	for(i=0;i<9;i++)
+//	for(i=0;i<=8;i++)
 //	{
-//		for(j=i+1;j<10;j++)
+//		for(j=i+1;j<=9;j++)
 //		{
 //			if(a[i]>a[j])
 //			{
@@ -97,27 +20,23 @@
 //			}
 //		}
 //	}
-//	printf("从小到大排序为：\n");
-//	for(i=0;i<10;i++)
+//	printf("排列如下：\n");
+//	for(i=0;i<=9;i++)
 //	{
-//		printf("%d\t",a[i]);
-//	}
-//	
+//		printf("%d\n",a[i]);
+//	}	
 //	return 0;
 //}
 
-////冒泡，沉底
 //#include <stdio.h>
 //int main()
 //{
-//	int a[10];
-//	int i,j,t;
-//	printf("请输入十个数字：\n");
+//	int a[10],i,j,t;
+//	printf("请输入10个数字：\n");
 //	for(i=0;i<10;i++)
 //	{
 //		scanf("%d",&a[i]);
 //	}
-//	
 //	for(j=0;j<9;j++)
 //	{
 //		for(i=0;i<9-j;i++)
@@ -131,14 +50,110 @@
 //		}
 //	}
 //	printf("排序如下：\n");
-//	for(i=0;i<10;i++)
+//	for(i=0;i<=9;i++)
 //	{
-//		printf("%d\t",a[i]);
+//		printf("%d\n",a[i]);
 //	}
-//	printf("\n");
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a[2][3]={{1,2,3},{4,5,6}};
+//	int b[3][2],i,j;
+//	for(i=0;i<2;i++)
+//	{
+//		for(j=0;j<3;j++)
+//		{
+//			printf("%5d",a[i][j]);
+//			b[j][i]=a[i][j];
+//		}
+//		printf("\n");
+//	}
+//	for(i=0;i<3;i++)
+//	{
+//		for(j=0;j<2;j++)
+//		{
+//			printf("%5d",b[i][j]);
+//		}
+//		printf("\n");
+//	}	
 //	return 0;
 // } 
 
+//#include <stdio.h>
+//int main ()
+//{
+//	int a[2][3]={{1,2,3},{4,5,6}};
+//	int b[3][2],i,j,array;
+//	for(i=0;i<2;i++)
+//	{
+//		for(j=0;j<3;j++)
+//		{
+//			printf("%5d",a[i][j]);
+//			b[j][i]=a[i][j];
+//		}
+//		printf("\n");
+//	}
+//	
+//	for(i=0;i<3;i++)
+//	{
+//		for(j=0;j<2;j++)
+//		{
+//			printf("%5d",b[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a[3][4]={{12,23,45,54},{22,56,78,89},{74,85,96,69}};
+//	int max,i,j,row,c;
+//	max=a[0][0];
+//	for(i=0;i<3;i++)
+//	{
+//		for(j=0;j<4;j++)
+//		{
+//			if(max<a[i][j])
+//			{
+//				max=a[i][j];
+//				row=i+1;
+//				c=j+1;
+//			}
+//		}
+//	}
+//	printf("第%d行%d列的数最大，是%d\n",row,c,max);
+//	return 0;
+//}
+
+
+ #include <stdio.h>
+int main()
+{
+	char string[100];
+	int i,num=0,word=0;
+	char c;
+	gets(string);
+	for(i=0;(c=string[i]) != '\0';i++)
+	{
+		if(c==' ')
+		{
+			word=0;
+		}
+		else if(word==0)
+		{
+			word=1;
+			num++;
+		}
+	}
+	printf("%d个单词\n",num);
+	return 0;
+}
 
 
 
@@ -151,12 +166,3 @@
 
 
 
-
-
-
-
-
-
-
-
- 
