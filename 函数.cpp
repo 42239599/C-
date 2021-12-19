6863d959249d1e776a304783406897e796c91829
42239599 <88647659+42239599@users.noreply.github.com>
2021-12-19 21:25:23 +0800

    Add files via upload

--- 函数.cpp
+++ 函数.cpp
@@ -1,201 +1,290 @@
-///函数
-//调用有参函数 
-////编写一程序，要求用户输入四个数字，输出前两个数中最大数，后两个数中最大数，以及四个数中最大数
 //#include <stdio.h>
-//int main()
+//void zb(int key,int a[],int n)
 //{
-//	float max(float x,float y);
-//	float a[4];
-//	int i;
-//	float t,u,v;
-//	for(i=0;i<4;i++)
+//	int i,mid,low,high,count1=0,count=0;
+//	low=0;
+//	high=n-1;
+//	while(high>=low)
 //	{
-//		printf("请输入第%d个数字；",i+1);
-//		scanf("%f",&a[i]);
+//		count++;
+//		mid=(low+high)/2;
+//		if(key<a[mid])
+//		{
+//			high=mid-1;
+//		}
+//		else if(key>a[mid])
+//		{
+//			low=mid+1;
+//		}
+//		if(key==a[mid])
+//		{
+//		printf("找到了，共查找了%d次，a[%d]=%d\n",count,mid,key);
+//		count1++; 
+//		break;	
+//		}
+//		if(count1==1)
+//		{
+//			printf("输入有误\n");
+//		}	
 //	}
-//	t=max(a[0],a[1]);
-//	u=max(a[2],a[3]);
-//	v=max(t,u);
-//	
-//	printf("前两个最大值为：%5.2f\n",t);
-//	printf("后两个最大值为：%5.2f\n",u);	
-//	printf("四数中最大值为：%5.2f\n",v);	
-//	return 0;
-// } 
-//float max(float x,float y)
-//{
-//	float zuidazhi;
-//	if(x>y)
-//	{
-//		zuidazhi = x;
-//	}
-//	else
-//	{
-//		zuidazhi = y ;
-//	}
-//	return(zuidazhi);
-//}
-
-////有两个小组，分别有5名学生和10名学生，请编程输入这些学生的成绩，并调用一个aver函数求这两个小组的平均分
-//#include <stdio.h>
-//int main()
-//{
-//	float aver(float a[],int n);
-//	float zu1[5],zu2[10];
-//	int i;
-//	printf("请输入1组的成绩:\n");
-//	for(i=0;i<5;i++)
-//	{
-//		scanf("%f",&zu1[i]);
-//	}
-//	printf("请输入2组的成绩:\n");
-//	for(i=0;i<10;i++)
-//	{
-//		scanf("%f",&zu2[i]);
-//	}	
-//	
-//	
-//	printf("1组平均分为：%f",aver(zu1,5));
-//	printf("2组平均分为：%f",aver(zu2,10));	
-//	return 0;
 //}
 //
-//float aver(float a[],int n)
-//{
-//	int i;
-//	float sum=a[0],pingjunfen;
-//	for(i=1;i<n;i++)
-//	{
-//		sum = sum + a[1];
-//	}
-//	pingjunfen =sum/n;
-//	return (pingjunfen);
-//	
-//}
-
-////调用无参函数 
-//请编程输入十个数，并将这十个数由小到大排序
-//#include <stdio.h>
 //int main()
 //{
-//	void paixu(int a[],int n);
-//	int i,a[10];
-//	printf("请输入十个数:\n");
-//	for(i=0;i<10;i++)
+//	void zb(int key,int a[],int n);
+//	int a[100],i,n,key;
+//	printf("请输入元素个数：\n");
+//	scanf("%d",&n);
+//	printf("按照从小到大顺序输入：\n");
+//	for(i=0;i<n;i++)
 //	{
 //		scanf("%d",&a[i]);
 //	}
-//	
-//	paixu(a,10);
-//	printf("排序如下：\n");
-//	for(i=0;i<10;i++)
-//	{
-//		printf("%d\t",a[i]);
-//	}
+//	printf("请输入要查找的元素：\n");
+//	scanf("%d",&key);
+//	zb(key,a,n);
 //	printf("\n");
 //	return 0;
-// } 
-//void paixu(int a[],int n)
-//{
-//	int i,j,t;
-//	for(j=0;j<n-1;j++)
-//	{
-//		for(i=0;i<n-1;i++)
-//		{
-//			if(a[i]>a[i+1])
-//			{
-//				t=a[i];
-//				a[i]=a[i+1];
-//				a[i+1]=t;
-//			}
-//		}
-//	}//冒泡排序 
-//	
 //}
 
-//////函数的嵌套
-////请输入4个整数，并找出其中最大数 
 //#include <stdio.h>
+//int  main()
+//{
+//	void print_star();
+//	void print_message();
+//	print_star();
+//	print_message();
+//	print_star();
+//	return 0;
+//}
+//void print_star()
+//{
+//	printf("************************\n");
+//}
+//void print_message()
+//{
+//	printf("how do you do !\n");
+//}
+
+//#include <stdio.h>
+//int max(int x,int y)
+//{
+//	int z;
+//	z=x>y?x:y;
+//	return(z);
+//}
+//int main()
+//{
+//	int max(int x,int y);
+//	int a,b,c;
+//	printf("请输入两个要比较大小的数：\n");
+//	scanf("%d %d",&a,&b);
+//	c=max(a,b);
+//	printf("%d\n",c);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//float add(float x,float y)
+//{
+//	float z;
+//	z=x+y;
+//	return z;
+//}
+//
+//int main()
+//{
+//	float add(float x,float y);
+//	float a,b,c;
+//	printf("请输入两个数：\n");
+//	scanf("%f %f",&a,&b);
+//	c=add(a,b);
+//	printf("两者的值为：%f\n",c);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int max4(int a,int b,int c,int d)
+//{
+//	int max2(int a,int b);
+//	int m;
+//	m=max2(max2(max2(a,b),c),d);
+//	return m;
+//}
+//int max2(int a,int b)
+//{
+//	return(a>b?a:b);
+//}
+//
 //int main()
 //{
 //	int max4(int a,int b,int c,int d);
-//	int a,b,c,d,zuidashu;
-//	printf("请输入四个数字:\n");
+//	int a,b,c,d,max;
+//	printf("请输入4个数字：\n");
 //	scanf("%d %d %d %d",&a,&b,&c,&d);
-//	
-//	zuidashu = max4(a,b,c,d);
-//	
-//	printf("最大值：%d\n",zuidashu);
-//	
+//	max=max4(a,b,c,d);
+//	printf("四个数中的最大数为：%d\n",max);
 //	return 0;
 //}
-//int max4(int a,int b,int c,int d)
-//{
-//	int max2(int x,int y);
-//	return(max2(max2(max2(a,b),c),d));
-//}
-//int max2(int x,int y)
-//{
-//	return(x>y?x:y);
-//}
 
 
-////函数的递归
-//有5个学生，第五个比第四个大2岁，第四比第三大2岁，第三比第二大2岁，第二比第一大2岁，假设第一个10岁，请编程计算第五个为学生年龄
-//
 //#include <stdio.h>
 //int main()
 //{
 //	int age(int n);
-//	printf("第五个学生年龄为%d\n",age(5));	
+//	printf("第五个学生的年龄是：%d\n",age(5));
 //	return 0;
-// } 
+//}
+//
 //int age(int n)
 //{
-//	int nianling;
+//	int c;
 //	if(n==1)
-//	{
-//		nianling =10;
-//	}
+//		c=10;
 //	else
-//	{
-//		nianling = age(n-1)+2;
-//		return (nianling);
-//	}
-//	
+//		c=age(n-1)+2;
+//	return c;
 //}
 
-////用递归的方法求阶乘 n!
+
+//#include <stdio.h>
+//int jc(int n)
+//{
+//	int a;
+//	if(n==1)
+//		a=1;
+//	else
+//		a=jc(n-1)*n;
+//	return a;		
+//}
+//
+//int main()
+//{
+//	int jc( int n);
+//	int n,c;
+//	printf("请输入要阶乘的数：\n");
+//	scanf("%d",&n);
+//	c=jc(n);
+//	printf("结果是：%d\n",c); 
+//	
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int jc(int n)
+//{
+//	int  f;
+//	if(n<0)
+//		printf("您的输入有误\n");
+//	else if(n==0 || n==1)	
+//		f=1;
+//	else
+//		f=jc(n-1)*n;	
+//	return f;
+//}
+//int main()
+//{
+//	int jc(int n);
+//	int n,c;
+//	printf("请输入要阶乘的数字：\n");
+//	scanf("%d",&n);
+//	c=jc(n);
+//	printf("%d! = %d",n,c);
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a=1, n,i;
+//	printf("请输入要阶乘的数：\n");
+//	scanf("%d",&n);
+//	if(n==1)
+//		a=1;
+//	else
+//	{
+//		for(i=n;i>0;i--)
+//		{
+//			a=a*i;
+//		}		
+//	}
+//	printf("%d! = %d\n",n,a);
+//	return 0;
+//}
+
 #include <stdio.h>
-int main()
+#include <string.h> 
+
+void hannuo(int n,char one,char two,char three)
 {
-	int jiecheng(int n);
-	int n;
-	printf("请输入一个整数：\n");
-	scanf("%d",&n);
-	
-	printf("%d 的阶乘是：%d\n",n,jiecheng(n));
-	
-	return 0;
- } 
-int jiecheng(int n)
-{
-	int jieguo;
-	if(n<0)
-	{
-		printf("输入的数字不对\n");
-	}
-	if(n==0 || n==1)
-	{
-		jieguo = 1;
-	}
+	void move(char x,char y);
+	if(n==1)
+		move(one,three);
 	else
 	{
-		jieguo = jiecheng(n-1)*n;
-	}
-	return (jieguo);
+		hannuo(n-1,one,three,two);
+		move(one,three);
+		hannuo(n-1,two,one,three);
+	}	
+}
+void move(char x,char y)
+{
+	printf("%c---->%c\n",x,y);
+}
+int main()
+{
+	int n;
+	printf("请输入你的数字：\n");
+	scanf("%d",&n);
+	hannuo(n,'A','B','C');
+	return 0;
 }
 
+//#include <stdio.h>
+//#include <string.h>
+///*
+// 算法思路：1将 n-1个盘子先放到B座位上
+//          2.将A座上地剩下的一个盘移动到C盘上
+//          3、将n-1个盘从B座移动到C座上
+//*/
+////函数声明
+//void move(char x, char y);
+//void hannuo(int n,char one ,char two,char three)
+//{
+//  if(n==1)move(one, three); //递归截止条件
+//  else
+//{
+//  hannuo(n-1,one ,three,two);//将 n-1个盘子先放到B座位上
+//  move(one,three);//将A座上地剩下的一个盘移动到C盘上
+//  hannuo(n-1,two,one,three);//将n-1个盘从B座移动到C座上
+// 
+//}
+//}
+//void move(char x,char y)
+//{
+// printf("%c--->%c\n",x,y);
+//}
+// 
+//int main()
+//{
+// int n;
+// printf("input your number");
+// scanf("%d",&n);
+// hannuo(n,'A','B','C');
+// return 0;
+//}
 
 
- 
+
+
+
+
+
+
+
+
+
+
+
+
