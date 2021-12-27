0def1fd2a54772b6f84f603bf43c087715d7398e
42239599 <88647659+42239599@users.noreply.github.com>
2021-12-27 21:37:36 +0800

    Add files via upload

--- 指针.cpp
+++ 指针.cpp
@@ -1,312 +1,24 @@
-//#include <stdio.h> 
+//# include <stdio.h>
 //int main()
 //{
-//	void exchange(int *q1,int *q2,int *q3);
-//	int a,b,c;
-//	int *p1,*p2,*p3;
-//	printf("请输入3个数字：\n");
-//	scanf("%d %d %d",&a,&b,&c);
-//	p1=&a;p2=&b;p3=&c;
-//	exchange(p1,p2,p3);
-//	printf("排序后的结果是：%d	%d	%d\n",a,b,c);
-//	return 0;
-//}
-//void exchange(int *q1,int *q2,int *q3)
-//{
-//	void swap(int *qt1,int *qt2);
-//	if(*q1<*q2)	swap(q1,q2);
-//	if(*q1<*q3)	swap(q1,q3);
-//	if(*q2<*q3)	swap(q2,q3);
-//}
-//void swap(int *qt1,int *qt2)
-//{
-//	int temp;
-//	temp=*qt1;
-//	*qt1=*qt2;
-//	*qt2=temp;
-//}
-
-/**************************
-有一个整型数组a，有10个元素，要求输出数组中的全部元素 
-1、下标法
-2、通过数组名计算数组元素地址，找出元素的值 
-3、 用指针变量指向数组元素 
-*****************************/ 
-//下标法
-//#include <stdio.h> 
-//int main()
-//{
-//	int a[10],i;
-//	printf("请输入十个数字：\n");
-//	for(i=0;i<10;i++)
-//	{
-//		scanf("%d",&a[i]);
-//	 } 
-//	printf("输出：\n");
-//	for(i=0;i<10;i++)
-//	{
-//		printf("%d\t",a[i]);
-//	}
-//	printf("\n");
-//	return 0;
-//}
-//地址法 
-//#include <stdio.h>
-//int main()
-//{
+//	void sorts(int a[],int n);
 //	int a[10],i;
 //	printf("请输入十个数：\n");
 //	for(i=0;i<10;i++)
 //	{
 //		scanf("%d",&a[i]);
 //	}
-//	printf("请输出各个数字：\n");
-//	for(i=0;i<10;i++)
-//	{
-//		printf("%d\t",*(a+i));
-//	}
-//	printf("\n");
-//	return 0;
-//}
-//指针变量指向数组元素
-//#include <stdio.h> 
-//int main()
-//{
-//	int a[10],i,*p;
-//	printf("请输入十个数字：\n");
-//	for(i=0;i<10;i++)
-//	{
-//		scanf("%d",&a[i]);
-//	}
-//	printf("请输出：\n");
-//	for(p=a;p<(a+10);p++)
-//	{
-//		printf("%d\t",*p);
-//	}
-//	return 0;
-//}
-
-//#include <stdio.h>
-//int main() 
-//{
-//	int a[10],i,*p=a;
-//	printf("请输入十个数：\n");
-//	for(i=0;i<10;i++)
-//	{
-//		scanf("%d",p++);
-//	}
-//	p=a;
-//	printf("输出结果为：\n");
-////	for(i=0;i<10;i++,p++)
-////	{
-////		printf("%d\t",*p);
-////	}
-//	for(i=0;i<10;i++)
-//	{
-//		printf("%d\t",*p++);
-//	}
-//	printf("\n");
-//	return 0;
-//}
-
-//原程序 
-//#include <stdio.h>
-//int main()
-//{
-//	int a[5],i,j,t,b;
-//	printf("请输入5个数：\n");
-//	for(i=0;i<5;i++)
-//	{
-//		scanf("%d",&a[i]);
-//	}
-//	b=5/2;
-//	for(i=0;i<=b;i++)
-//	{
-//		j=5-1-i;
-//		t=a[i];
-//		a[i]=a[j];
-//		a[j]=t;
-//	}
-//	printf("反序输出为：\n");	
-//	for(i=0;i<5;i++)
-//	{
-//		printf("%d\t",a[i]);
-//	}
-//	printf("\n");
-//	return 0;
-//}
-//数组函数方法 
-//#include <stdio.h>
-//int main()
-//{
-//	void fanxu(int a[],int n);
-//	int a[5],i;
-//	printf("请输入5个数字：\n");
-//	for(i=0;i<5;i++)
-//	{
-//		scanf("%d",&a[i]);
-//	}
-//	fanxu(a,5);
-//	printf("倒序为：\n");
-//	for(i=0;i<5;i++)
-//	{
-//		printf("%d\t",a[i]);
-//	}
-//	printf("\n");
-//	return 0;
-//}
-//void fanxu(int a[],int n)
-//{
-//	int i,t,b,j;
-//	b=(n-1)/2;
-//	for(i=0;i<=b;i++)
-//	{
-//		j=n-1-i;
-//		t=a[i];
-//		a[i]=a[j];
-//		a[j]=t;
-//	}
-//	return ;
-//}
-//数组名作函数参数（指针） 
-//#include <stdio.h>
-//int main()
-//{
-//	void daoxu(int *x,int n);
-//	int a[10],i;
-//	printf("请输入5个数字：\n");
-//	for(i=0;i<5;i++)
-//	{
-//		scanf("%d",&a[i]);
-//	}
-//	daoxu(a,5);
-//	printf("倒序后是：\n");
-//	for(i=0;i<5;i++)
-//	{
-//		printf("%d\t",a[i]);
-//	}
-//	return 0;
-//}
-//void daoxu(int *x,int n)
-//{
-//	int t,*i,*j,m=(n-1)/2,*p;
-//	i=x;j=x+n-1;p=x+m;
-//	for(;i<=p;i++,j--)
-//	{
-//		t=*i;
-//		*i=*j;
-//		*j=t;
-//	}
-//	return ;
-//}
-//指针变量作实参 
-//#include <stdio.h>
-//int main()
-//{
-//	void daoxu(int *x,int n);
-//	int a[5],*p=a,i;
-//	printf("请输入5个数字：\n");
-//	for(i=0;i<5;i++,p++)
-//	{
-//		scanf("%d",p);
-//	}
-//	p=a;
-//	daoxu(p,5);
-//	printf("倒序输出为：\n");
-//	for(p=a;p<a+5;p++)
-//	{
-//		printf("%d\t",*p);
-//	}
-//	printf("\n");
-//	return 0;
-//}
-//void daoxu(int *x,int n)
-//{
-//	int m=(n-1)/2,*i,*j,t,*p;
-//	i=x;j=x+n-1;p=x+m;
-//	for(;i<p;i++,j--)
-//	{
-//		t=*i;
-//		*i=*j;
-//		*j=t;
-//	}
-//	return ;
-//}
-
-//#include <stdio.h> 
-//int main()
-//{
-//	void daoxu(int *x,int n);
-//	int a[5],i,*p=a;
-//	printf("请输入5个数：\n");
-//	for(i=0;i<5;i++,p++)
-//	{
-//		scanf("%d",p);
-//	}
-//	p=a;
-//	daoxu(p,5);
-//	printf("倒序如下：\n");
-//	for(p=a;p<a+5;p++)
-//	{
-//		printf("%d\t",*p);
-//	}
-//	return 0;
-//}
-//void daoxu(int *x,int n)
-//{
-//	int *p,t,*i,*j,m=(n-1)/2;
-//	i=x;j=x+n-1;p=x+m;
-//	for(i=x;i<p;i++,j--)
-//	{
-//		t=*i;
-//		*i=*j;
-//		*j=t;
-//	}
-//	return;
-//}
-//选择法 
-//#include <stdio.h>
-//int main()
-//{
-//	int a[10],i,j;
-//	printf("请输入要排序的10位数：\n");
-//	for(i=0;i<10;i++)
-//	{
-//		scanf("%d",&a[i]);
-//	}
-//
-//	for(i=0;i<9;i++)
-//	{
-//		for(j=i+1;j<10;j++)
-//		{
-//			if(a[i]>a[j])
-//			{
-//				int t;
-//				t=a[i];
-//				a[i]=a[j];
-//				a[j]=t;
-//			}
-//		}
-//	}
-//	printf("排序如下：\n");	
+//	printf("排序后如下：\n");
+//	sorts(a,10);
 //	for(i=0;i<10;i++)
 //	{
 //		printf("%d\t",a[i]);
 //	}
-//	printf("\n");
 //	return 0;
 //}
-
-//#include <stdio.h>
-//int main()
+//void sorts(int a[],int n)
 //{
-//	int a[10],i,j,t;
-//	printf("请输入十个数字：\n");
-//	for(i=0;i<10;i++)
-//	{
-//		scanf("%d",&a[i]);
-//	}
-//	
+//	int i,j,t;
 //	for(j=0;j<9;j++)
 //	{
 //		for(i=0;i<9-j;i++)
@@ -319,14 +31,249 @@
 //			}
 //		}
 //	}
-//	printf("排序如下：\n");
+//	return;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	void sorts(int *x,int n);
+//	int a[10],*p=a;
+//	printf("请输入十个数：\n");
+//	for(p=a;p<a+10;p++)
+//	{
+//		scanf("%d",p);
+//	}
+//	p=a;
+//	sorts(a,10);
+//	printf("排序后如下：\n");
+//	for(p=a;p<a+10;p++)
+//	{
+//		printf("%d\t",*p);
+//	}
+//	return 0;
+//}
+//void sorts(int *x,int n)
+//{
+//	int *i,*j,t;
+//	i=x,j=x;
+//	for(i=x;i<x+n-1;i++)
+//	{
+//		for(j=i+1;j<x+n;j++)
+//		{
+//			if(*i<*j)
+//			{
+//				t=*i;
+//				*i=*j;
+//				*j=t;
+//			}
+//		}
+//	}
+//	return;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	void sort(int a[],int n);
+//	int a[10],i,*p=a;
+//	printf("请输入十个数字：\n");
 //	for(i=0;i<10;i++)
 //	{
-//		printf("%d\t",a[i]);
+//		scanf("%d",p);
+//		p++;
+//	}
+//	p=a;
+//	sort(p,10);
+//	printf("排序后是：\n");
+//	for(p=a,i=0;i<10;i++)
+//	{
+//		printf("%d\t",*p);
+//		p++;
 //	}
 //	printf("\n");
 //	return 0;
 //}
+//void sort(int a[],int n)
+//{
+//	int i,j,t;
+//	for(i=0;i<n-1;i++)
+//	{
+//		for(j=i+1;j<n;j++)
+//		{
+//			if(a[i]<a[j])
+//			{
+//				t=a[i];
+//				a[i]=a[j];
+//				a[j]=t;
+//			}
+//		}
+//	}	
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	void sort(int *x,int n);
+//	int a[10],i,*p=a;
+//	printf("请输入十个数字：\n");
+//	for(i=0;i<10;i++)
+//	{
+//		scanf("%d",p);
+//		p++;
+//	}
+//	p=a;
+//	sort(p,10);
+//	printf("排序后是：\n");
+//	for(p=a,i=0;i<10;i++)
+//	{
+//		printf("%d\t",*p);
+//		p++;
+//	}
+//	return 0;
+//}
+//void sort(int *x,int n)
+//{
+//	int i,j,t;
+//	for(i=0;i<n-1;i++)
+//	{
+//		for(j=i+1;j<n;j++)
+//		{
+//			if(*(x+i)<*(x+j))
+//			{
+//				t=*(x+i);
+//				*(x+i)=*(x+j);
+//				*(x+j)=t;
+//			}
+//		}
+//	}	
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a[3][4]={{10,22,33,54},{9,5,6,7},{12,10,20,30}}	;
+//	printf("%d",*a);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a[3][4]={1,3,5,7,9,11,13,15,17,19,21,23};
+//	int *p;
+//	for(p=a[0];p<a[0]+12;p++)
+//	{
+//		if((p-a[0])%4==0)	printf("\n");
+//		printf("%4d",*p);
+//	}
+//	int (*p)[4],i,j;
+//	p=a;
+//	printf("请输入要查找的行和列：\n");
+//	scanf("%d %d",&i,&j);
+//	printf("a[%d][%d]的值是：%d",i,j,*(*(p+i)+j));
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	void average(float *p,int n);
+//	void search(float (*p)[4],int n);
+//	float score[3][4]={{74,78,87,90},{98,68,74,88},{99,56,98,70}};
+//	average(*score ,12);
+//	search(score,2);
+//	return 0;
+//}
+//void average(float *p,int n)
+//{
+//	float *p_end;
+//	float sum=0,aver;
+//	p_end=p+n-1;
+//	for(;p<=p_end;p++)
+//	{
+//		sum=sum+(*p);
+//	}
+//	aver=sum/n;
+//	printf("平均分为：%5.2f\n",aver);
+//}
+//void search(float (*p)[4],int n)
+//{
+//	int i;
+//	printf("the score of NO.%d are:\n",n);
+//	for(i=0;i<4;i++)
+//	{
+//		printf("%10.2f",*(*(p+n)+i));
+//	}
+//	printf("\n");
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	void average(float *q,int n);
+//	float score[3][4]={{75,84,74,96},{87,98,75,84},{74,99,85,76}};
+//	float *p;
+//	p=&score[0][0];
+//	printf("总平均分为：\n");
+//	average(p,12);
+//	return 0;
+//}
+//void average(float *q,int n)
+//{
+//	float sum=0,aver;
+//	int i;
+//	for(i=0;i<n;i++)
+//	{
+//		sum+=*(q+i);
+//	}
+//	aver=sum/n;
+//	printf("%5.2f\n",aver);
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	void average(float *x,int n);
+//	float a[3][4]={{75,84,74,96},{87,98,75,84},{74,99,85,76}};
+//	float *p;
+//	p=&a[0][0];
+//	average(p,12);
+//	return 0;
+//}
+//void average(float *x,int n)
+//{
+//	float sum=0,aver;
+//	int i;
+//	for(i=0;i<n;i++)
+//	{
+//		sum+=*(x+i);
+//	}
+//	aver=sum/n;
+//	printf("%5.2f\n",aver);
+//}
+
+#include <stdio.h>
+int main()
+{
+	void average(float *p,int n);
+	float a[3][4]={{75,84,74,96},{87,98,75,84},{74,99,85,76}};
+	average(*a,12);
+	return 0;
+}
+void average(float *p,int n)
+{
+	float sum=0,aver;
+	float *p_end;
+	p_end=p+n-1;
+	for(;p<=p_end;p++)
+	{
+		sum+=*(p);
+	}
+	aver=sum/n;
+	printf("%5.2f\n",aver);
+}
 
 
 
