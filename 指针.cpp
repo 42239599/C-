1c5a5b8bba359664829b68dc8d1678a4c8df1c74
42239599 <88647659+42239599@users.noreply.github.com>
2021-12-28 19:14:28 +0800

    Add files via upload

--- 指针.cpp
+++ 指针.cpp
@@ -1,279 +1,208 @@
-//# include <stdio.h>
+//#include <stdio.h> 
 //int main()
 //{
-//	void sorts(int a[],int n);
-//	int a[10],i;
-//	printf("请输入十个数：\n");
-//	for(i=0;i<10;i++)
-//	{
-//		scanf("%d",&a[i]);
-//	}
-//	printf("排序后如下：\n");
-//	sorts(a,10);
-//	for(i=0;i<10;i++)
-//	{
-//		printf("%d\t",a[i]);
-//	}
-//	return 0;
-//}
-//void sorts(int a[],int n)
-//{
-//	int i,j,t;
-//	for(j=0;j<9;j++)
-//	{
-//		for(i=0;i<9-j;i++)
-//		{
-//			if(a[i]>a[i+1])
-//			{
-//				t=a[i];
-//				a[i]=a[i+1];
-//				a[i+1]=t;
-//			}
-//		}
-//	}
-//	return;
-//}
-
-//#include <stdio.h>
-//int main()
-//{
-//	void sorts(int *x,int n);
-//	int a[10],*p=a;
-//	printf("请输入十个数：\n");
-//	for(p=a;p<a+10;p++)
-//	{
-//		scanf("%d",p);
-//	}
-//	p=a;
-//	sorts(a,10);
-//	printf("排序后如下：\n");
-//	for(p=a;p<a+10;p++)
-//	{
-//		printf("%d\t",*p);
-//	}
-//	return 0;
-//}
-//void sorts(int *x,int n)
-//{
-//	int *i,*j,t;
-//	i=x,j=x;
-//	for(i=x;i<x+n-1;i++)
-//	{
-//		for(j=i+1;j<x+n;j++)
-//		{
-//			if(*i<*j)
-//			{
-//				t=*i;
-//				*i=*j;
-//				*j=t;
-//			}
-//		}
-//	}
-//	return;
-//}
-
-//#include <stdio.h>
-//int main()
-//{
-//	void sort(int a[],int n);
-//	int a[10],i,*p=a;
-//	printf("请输入十个数字：\n");
-//	for(i=0;i<10;i++)
-//	{
-//		scanf("%d",p);
-//		p++;
-//	}
-//	p=a;
-//	sort(p,10);
-//	printf("排序后是：\n");
-//	for(p=a,i=0;i<10;i++)
-//	{
-//		printf("%d\t",*p);
-//		p++;
-//	}
-//	printf("\n");
-//	return 0;
-//}
-//void sort(int a[],int n)
-//{
-//	int i,j,t;
-//	for(i=0;i<n-1;i++)
-//	{
-//		for(j=i+1;j<n;j++)
-//		{
-//			if(a[i]<a[j])
-//			{
-//				t=a[i];
-//				a[i]=a[j];
-//				a[j]=t;
-//			}
-//		}
-//	}	
-//}
-
-//#include <stdio.h>
-//int main()
-//{
-//	void sort(int *x,int n);
-//	int a[10],i,*p=a;
-//	printf("请输入十个数字：\n");
-//	for(i=0;i<10;i++)
-//	{
-//		scanf("%d",p);
-//		p++;
-//	}
-//	p=a;
-//	sort(p,10);
-//	printf("排序后是：\n");
-//	for(p=a,i=0;i<10;i++)
-//	{
-//		printf("%d\t",*p);
-//		p++;
-//	}
-//	return 0;
-//}
-//void sort(int *x,int n)
-//{
-//	int i,j,t;
-//	for(i=0;i<n-1;i++)
-//	{
-//		for(j=i+1;j<n;j++)
-//		{
-//			if(*(x+i)<*(x+j))
-//			{
-//				t=*(x+i);
-//				*(x+i)=*(x+j);
-//				*(x+j)=t;
-//			}
-//		}
-//	}	
-//}
-
-//#include <stdio.h>
-//int main()
-//{
-//	int a[3][4]={{10,22,33,54},{9,5,6,7},{12,10,20,30}}	;
-//	printf("%d",*a);
-//	return 0;
-//}
-
-//#include <stdio.h>
-//int main()
-//{
-//	int a[3][4]={1,3,5,7,9,11,13,15,17,19,21,23};
-//	int *p;
-//	for(p=a[0];p<a[0]+12;p++)
-//	{
-//		if((p-a[0])%4==0)	printf("\n");
-//		printf("%4d",*p);
-//	}
-//	int (*p)[4],i,j;
-//	p=a;
-//	printf("请输入要查找的行和列：\n");
-//	scanf("%d %d",&i,&j);
-//	printf("a[%d][%d]的值是：%d",i,j,*(*(p+i)+j));
-//	return 0;
-//}
-
-
-//#include <stdio.h>
-//int main()
-//{
+//	void search_1(float (*p)[4],int n);
 //	void average(float *p,int n);
 //	void search(float (*p)[4],int n);
-//	float score[3][4]={{74,78,87,90},{98,68,74,88},{99,56,98,70}};
-//	average(*score ,12);
-//	search(score,2);
+//	int a;
+//	float score[3][4]={{74,75,78,95},{85,87,58,54},{58,99,97,88}};
+//	printf("总平均分是：\n");
+//	average(*score,12);	
+//	printf("请输入要查找的学生序号：\n");
+//	scanf("%d",&a);
+//	search(score,a-1);
+//	search_1(score,3);
 //	return 0;
 //}
 //void average(float *p,int n)
 //{
+//	float aver,sum=0;
 //	float *p_end;
-//	float sum=0,aver;
 //	p_end=p+n-1;
-//	for(;p<=p_end;p++)
+//	for(;p<p_end;p++)
 //	{
-//		sum=sum+(*p);
+//		sum+=*p;
 //	}
 //	aver=sum/n;
-//	printf("平均分为：%5.2f\n",aver);
+//	printf("%5.2f",aver);
 //}
 //void search(float (*p)[4],int n)
 //{
 //	int i;
-//	printf("the score of NO.%d are:\n",n);
+//	printf("第%d名学生的成绩是：\n",n+1);
 //	for(i=0;i<4;i++)
 //	{
-//		printf("%10.2f",*(*(p+n)+i));
+//		printf("%5.2f\t",*(*(p+n)+i));
 //	}
 //	printf("\n");
 //}
+//void search_1(float (*p)[4],int n)
+//{
+//	int i,j,flat;
+//
+//	for(i=0;i<n;i++)
+//	{
+//		flat = 0;
+//		for(j=0;j<4;j++)
+//		{
+//			if(*(*(p+i)+j)<60)
+//			flat=1;
+//		}
+//		if(flat == 1)
+//		{
+//			printf("第%d名学生有成绩不及格，成绩分别如下：\n",i+1);
+//			for(j=0;j<4;j++)
+//			{
+//				printf("%5.2f\t",*(*(p+i)+j));
+//			}
+//		}
+//		printf("\n");		
+//	}
+//}
 
 //#include <stdio.h>
 //int main()
 //{
-//	void average(float *q,int n);
-//	float score[3][4]={{75,84,74,96},{87,98,75,84},{74,99,85,76}};
-//	float *p;
-//	p=&score[0][0];
-//	printf("总平均分为：\n");
-//	average(p,12);
-//	return 0;
+//	void average(float *p,int n); 
+//	void search(float (*p)[4],int n);
+//	void search_1(float (*p)[4],int n);
+//	int a;
+//	float score[3][4]={{99,98,99,40},{75,85,89,68},{59,85,98,76}};
+//	average(*score,12);	//求平均分 
+//	printf("请输入要查找的成绩的排号：\n");
+//	scanf("%d",&a);
+//	search(score,a-1);	//查找对应同学的成绩 
+//	search_1(score,3);	//查找成绩不及格的全部成绩 
+//	
 //}
-//void average(float *q,int n)
+//void average(float *p,int n)
 //{
-//	float sum=0,aver;
-//	int i;
-//	for(i=0;i<n;i++)
+//	float sum=0,aver, *p_end;
+//	p_end=p+n-1;
+//	for(;p<p_end;p++)
 //	{
-//		sum+=*(q+i);
+//		sum+=*p;
 //	}
 //	aver=sum/n;
-//	printf("%5.2f\n",aver);
+//	printf("总平均分为：%5.2f\n",aver);
+//}
+//
+//void search(float (*p)[4],int n)
+//{
+//	int i;
+//	printf("第%d名同学的成绩分别如下：\n",n+1);
+//	for(i=0;i<4;i++)
+//	{
+//		printf("%5.2f\t",*(*(p+n)+i));
+//	}
+//	printf("\n");
+//}
+//
+//void search_1(float (*p)[4],int n)
+//{
+//	int i,j,flag;
+//	for(i=0;i<n;i++)
+//	{
+//		flag=0;
+//		for(j=0;j<4;j++)
+//		{
+//			if(*(*(p+i)+j)<60)
+//			flag=1;
+//		}
+//		if(flag==1)
+//		{
+//			printf("序号为%d的同学有不及格的科目，成绩分别如下：\n",i+1);
+//			for(j=0;j<4;j++)
+//			{
+//				printf("%5.2f\t",*(*(p+i)+j));
+//			}
+//		}
+//		printf("\n");
+//	}
 //}
 
 //#include <stdio.h>
 //int main()
 //{
-//	void average(float *x,int n);
-//	float a[3][4]={{75,84,74,96},{87,98,75,84},{74,99,85,76}};
-//	float *p;
-//	p=&a[0][0];
-//	average(p,12);
+//	char string[]="i love China";
+//	printf("%s\n",string);
+//	printf("%c\n",string[7]);
 //	return 0;
 //}
-//void average(float *x,int n)
+
+//#include <stdio.h>
+//int main()
 //{
-//	float sum=0,aver;
+//	char a[]="i love china!",b[20];//定义字符数组 
 //	int i;
-//	for(i=0;i<n;i++)
+//	for(i=0;*(a+i) != '\0';i++)
 //	{
-//		sum+=*(x+i);
+//		*(b+i)=*(a+i);				//将a[i]的值赋给b[i] 
 //	}
-//	aver=sum/n;
-//	printf("%5.2f\n",aver);
+//	*(b+i)='\0';					//在b数组的有效字符之后假'\0' 
+//	printf("string a is:%s\n",a);	//输出a数组的全部有效字符 
+//	printf("string b is:\n");
+//	for(i=0;*(b+i) != '\0';i++)
+//	{
+//		printf("%c",b[i]);			//逐个输出b数组中全部有效字符 
+//	}
+//	printf("\n");
+//	
+//	return 0;
 //}
 
-#include <stdio.h>
-int main()
-{
-	void average(float *p,int n);
-	float a[3][4]={{75,84,74,96},{87,98,75,84},{74,99,85,76}};
-	average(*a,12);
-	return 0;
-}
-void average(float *p,int n)
-{
-	float sum=0,aver;
-	float *p_end;
-	p_end=p+n-1;
-	for(;p<=p_end;p++)
-	{
-		sum+=*(p);
-	}
-	aver=sum/n;
-	printf("%5.2f\n",aver);
-}
+//#include <stdio.h> 
+//int main()
+//{
+//	char a[]="i am a student.",b[20];
+//	int i;
+//	for(i=0;*(a+i) != '\0';i++)
+//	{
+//		*(b+i)=*(a+i);
+//	}
+//	*(b+i)='\0';
+//	printf("string a is:%s\n",a);
+//	printf("string b is:\n");
+//	for(i=0;*(b+i) != '\0';i++)
+//	{
+//		printf("%c",b[i]);
+//	}
+//	printf("\n");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	char a[]="i am a student.",b[20],*pa,*pb;
+//	pa=a;pb=b;						//pa,pb分别指向a数组和b数组中的第一个元素 
+//	for(;*pa != '\0';pa++,pb++)		//pa,pab每次都自加1 
+//	{
+//		*pb=*pa;					//将pa所指向的元素赋值给pb所指向的元素 
+//	}
+//	*pb='\0';						//在复制所有有效字符之后加'\0' 
+//	printf("string a is:%s\n",a);	//输出a的元素 
+//	printf("string b is:%s\n",b);	//输出b的元素 
+//	return 0;
+//}
+
+//#include <stdio.h> 
+//int main()
+//{
+//	char a[]="i love china!",b[20],*p1,*p2;
+//	p1=a;p2=b;
+//	for(;*p1 != '\0';p1++,p2++)
+//	{
+//		*p2=*p1;
+//	}
+//	*p2='\0';
+//	printf("string a is:%s\n",a);
+//	printf("string b is:%s\n",b);
+//	return 0;
+//}
+
+
+
 
 
 
