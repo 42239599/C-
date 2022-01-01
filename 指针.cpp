7966aa4702afd8581a3054adef134cfa4ee16217
42239599 <88647659+42239599@users.noreply.github.com>
2022-01-01 22:35:26 +0800

    Add files via upload

--- 指针.cpp
+++ 指针.cpp
@@ -1,32 +1,26 @@
-//#include <stdio.h> 
+//#include <stdio.h>
 //int main()
 //{
-//	int fun(int x,int y,int (*p)(int,int));
 //	int max(int,int);
 //	int min(int,int);
-//	int add(int,int);
-//	int  a,b,n;
-//	printf("请输入两个数组：\n");
+//	int (*p)(int x,int y);
+//	int a,b,c,n;
+//	printf("请输入两个数字：\n");
 //	scanf("%d %d",&a,&b);
-//	printf("请输入你的选择1,2,或3\n");
+//	printf("请输入您的选择1或2：\n");
 //	scanf("%d",&n);
-//	if(n==1)	fun(a,b,max);
-//	else if(n==2)	fun(a,b,min);
-//	else if(n==3)	fun(a,b,add);
+//	if(n==1)	p=max;
+//	else if(n==2)	p=min;
+//	else printf("输入有误，请从重新输入!!\n");
+//	c=(*p)(a,b);
+//	printf("输出的结果是：%d\n",c);
 //	return 0;
-//}
-//int fun(int x,int y,int (*p)(int,int))
-//{
-//	int result;
-//	result=(*p)(x,y);
-//	printf("%d\n",result);
 //}
 //int max(int x,int y)
 //{
 //	int z;
 //	if(x>y)	z=x;
 //	else	z=y;
-//	printf("max=");
 //	return z;
 //}
 //int min(int x,int y)
@@ -34,66 +28,74 @@
 //	int z;
 //	if(x<y)	z=x;
 //	else	z=y;
-//	printf("min=");
 //	return z;
 //}
-//int add(int x,int y)
+
+//#include <stdio.h>
+//#include <string.h>
+//int main()
 //{
-//	int z;
-//	z=x+y;
-//	printf("add=");
-//	return z;
+//	void sort(char *name[],int n);
+//	void print(char *name[],int n);
+//	char *name[]={"fellow me","basic","great wall","fortran","computer designs"};
+//	int n=5;
+//	sort (name,n);
+//	print(name,n);
+//	return 0;
+//}
+//void sort(char *name[],int n)
+//{
+//	char *temp;
+//	int i,j,k;
+//	for(i=0;i<n-1;i++)
+//	{
+//		k=i;
+//		for(j=i+1;j<n;j++)
+//			if(strcmp(name[k],name[j])>0) k=j;
+//		if(k != i)
+//		{
+//			temp=name[i];
+//			name[i]=name[k];
+//			name[k]=temp;
+//		}
+//		
+//	}
+//}
+//void print(char *name[],int n)
+//{
+//	int i;
+//	for(i=0;i<n;i++)
+//	{
+//		printf("%s\n",name[i]);
+//	}
 //}
 
 //#include <stdio.h>
 //int main()
 //{
-//	float score[3][4]={{75,85,95,68},{87,89,98,85},{52,85,45,99}};
-//	float *search(float (*pointer)[4],int n);
-//	float *p;
-//	int n,i;
-//	printf("请输入要查找的序号：\n");
-//	scanf("%d",&n);
-//	printf("the score of NO.%d is:\n",n);
-//	p=search(score,n);
-//	for(i=0;i<4;i++)
+//	char *name[]={"flollow","basic","great wall","fortran","computer design"};
+//	char **p;
+//	int i;
+//	for(i=0;i<5;i++)
 //	{
-//		printf("%5.2f\t",*(p+i));
+//		p=name+i;
+//		printf("%s\n",*p);
 //	}
-//	printf("\n");
 //	return 0;
-//}
-//float *search(float (*pointer)[4],int n)
-//{
-//	float *pt;
-//	pt=*(pointer+n);
-//	return pt;
 //}
 
-//#include <stdio.h>
-//int main()
-//{
-//	float *search(float (*pointer)[4],int n);
-//	float score[3][4]={{75,85,95,68},{87,89,98,85},{52,85,45,99}};
-//	float *p;
-//	int i,a;
-//	printf("请输入要查找的序号：\n");
-//	scanf("%d",&a);
-//	printf("the score of NO.%d are:\n",a);
-//	p=search(score,a);
-//	for(i=0;i<4;i++)
-//	{
-//		printf("%5.2f\t",*(p+i));
-//	}
-//	printf("\n");
-//	return 0;
-//}
-//float *search(float (*pointer)[4],int n)
-//{
-//	float *pt;
-//	pt=*(pointer+n);
-//	return pt;
-//}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
