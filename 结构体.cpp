ecab7dbdc2918f25e60fef7cad81349a16f83461
42239599 <88647659+42239599@users.noreply.github.com>
2022-01-12 03:39:23 -0600

    Add files via upload

--- 结构体.cpp
+++ 结构体.cpp
@@ -1,161 +1,165 @@
 //#include <stdio.h> 
-//#define n 3
-//struct student
-//{
-//	char num[10];
-//	char name[20];
-//	float score[3];
-//	float aver;
-//};
-//int main()
-//{
-//	void input(struct student stu[]);
-//	struct student max(struct student stu[]);
-//	void print(struct student stu);
-//	struct student stu[n],*p=stu;
-//	input(p);
-//	print(max(p));
-//	return 0;
-//}
-//void input(struct student stu[])
-//{
-//	int i;
-//	for(i=0;i<n;i++)
-//	{
-//		printf("请输入第%d位学生的学号，名字和三科成绩：\n",i+1);
-//		scanf("%s %s %f %f %f",stu[i].num,stu[i].name,&stu[i].score[0],&stu[i].score[1],&stu[i].score[2]);
-//		stu[i].aver=(stu[i].score[0]+stu[i].score[1]+stu[i].score[2])/3.0;
-//	} 
-//}
-//struct student max(struct student stu[])
-//{
-//	int i,m;
-//	for(i=0;i<n;i++)
-//	{
-//		if(stu[i].aver>stu[m].aver)
-//		m=i;
-//	}
-//	return stu[m];
-//}
-//void print(struct student stud)
-//{
-//	printf("最高分成绩如下：\n");
-//	printf("学号:%s\n姓名:%s\n三科成绩:%5.2f\t%5.2f\t%5.2f\n平均分:%5.2f\n",stud.num ,stud.name ,stud.score [0],stud.score [1],stud.score [2],stud.aver );
-////}
-//
-//#include <stdio.h>
-//#define n 3
-//struct  student
-//{
-//	char num[7];
-//	char name[20];
-//	float score[3];
-//	float aver;
-//};
-//int main()
-//{
-//	void input(struct student stu[]);
-//	struct student max(struct student stu[]);
-//	void print(struct student stu);
-//	struct student stu[n],*p=stu;
-//	input(p);
-//	print(max(p));
-//	return 0;
-//}
-//void input(struct student stu[])
-//{
-//	int i;
-//	for(i=0;i<n;i++)
-//	{
-//		printf("请输入第%d位同学的信息：\n",i+1);
-//		scanf("%s %s %f %f %f",stu[i].num,stu[i].name,&stu[i].score[0],&stu[i].score[1],&stu[i].score[2]);
-//		stu[i].aver=(stu[i].score[0]+stu[i].score[1]+stu[i].score[2])/3.0;
-//	}
-//	
-//}
-//struct student max(struct student stu[])
-//{
-//	int i,m=0;
-//	for(i=0;i<n;i++)
-//	{
-//		if(stu[i].aver>stu[m].aver)
-//		m=i;
-//	}
-//	return stu[m];
-//}
-//void print(struct student stud)
-//{
-//	printf("成绩最高的信息如下：\n");
-//	printf("学号:%s\n姓名:%s\n三科成绩:%5.2f\t%5.2f\t%5.2f\n平均分:%5.2f\n",
-//	stud.num ,stud.name ,stud.score [0],stud.score [1],stud.score [2],stud.aver );
-//	
-//}
-
-//#include <stdio.h>
-//struct student			//声明结构体类型struct student 
+//struct
 //{
 //	int num;
-//	float score;
-//	struct student *next;
-//};
+//	char name[10];
+//	char sex;
+//	char job;
+//	union
+//	{
+//		int clas;
+//		char position[10];
+//	}category;
+//}person[2];
 //int main()
 //{
-//	struct student a,b,c,*p,*head;//定义3个结构体变量，a,b,c作为链表的结点 
-//	a.num =10086;a.score =90;//对结点a的num和score成员赋值 
-//	b.num =10010;b.score =94;//对结点b的num和score成员赋值 
-//	c.num =10000;c.score =96;//对结点c的num和score成员赋值 
-//	head=&a;		//将结点a的起始地址赋给头指针head 
-//	a.next =&b;		//将结点b的起始地址赋给a结点的next成员 
-//	b.next =&c;		//将结点c的起始地址赋给b结点的next成员 
-//	c.next =NULL;	//c结点的next成员不存放其他结点地址 
-//	p=head;			//p爷指向a结点 
-//	do
+//	int i;
+//	for(i=0;i<2;i++)
 //	{
-//		printf("%d\t%5.2f\n",p->num ,p->score );//输出p指向的结点的数据 
-//		p=p->next ;		//使p指向下一结点 
-//	}while(p!=NULL);	//输出完c结点后p的值为	null ，循环终止 
+//		printf("please input the data of person:\n");
+//		scanf("%d %s %c %c",&person[i].num,person[i].name,&person[i].sex,&person[i].job );
+//		if(person[i].job=='s')
+//			scanf("%d",&person[i].category.clas);
+//		else if(person[i].job=='t')
+//			scanf("%s",person[i].category.position);
+//		else
+//			printf("error!!\n");
+//	}
+//	printf("NO.	name	sex	job	class/posision\n");
+//	for(i=0;i<2;i++)
+//	{
+//		if(person[i].job =='s')
+//			printf("%d\t%s\t%c\t%c\t%d\n",person[i].num,person[i].name,person[i].sex,person[i].job,person[i].category.clas );
+//		else
+//			printf("%d\t%s\t%c\t%c\t%s\n",person[i].num,person[i].name,person[i].sex,person[i].job,person[i].category.position);
+//	}
+//	
 //	return 0;
 //}
 
-#include <stdio.h> 
-struct 
-{
-	int num;
-	char name[10];
-	char sex;
-	char job;
-	union
-	{
-		int clas;
-		char posision[10];
-	}category;
-}person[2];
+//#include <stdio.h>
+//int main()
+//{
+//	enum color{red,yellow,blue,white,black};	//声明枚举类型enum color 
+////	enum color i,j,k,pri;	//课本上的式子 
+//	int i,j,k,pri;		//定义枚举变量i,j,k,prin 
+//	int n,loop;
+//	n=0;
+//	for(i=red;i<=black;i++)							//外循环使i的值从red变到black 
+//	{
+//		for(j=red;j<=black;j++)						//中循环使j的值从red变到black 
+//		{
+//			if(i!=j)								//如果两球不同色 
+//			{
+//				for(k=red;k<=black;k++)				//内循环使k的值从red到black 
+//				{
+//					if((k!=i)&&(k!=j))				//如果3球颜色不同 
+//					{
+//						n=n+1;						//符合条件的次数加1 
+//						printf("%-4d",n);			//输出当前是第几个符合条件的组合 
+//						for(loop=1;loop<=3;loop++)	//先后对3个球分别处理 
+//						{
+//							switch(loop)			//loop的值从1变到3 
+//							{
+//								case 1:pri=i;break;	//loop的值为1时，把第一个球的颜色赋给Pri 
+//								case 2:pri=j;break;	//loop的值为2时，把第二个球的颜色赋给Pri 
+//								case 3:pri=k;break;	//loop的值为3时，把第三个球的颜色赋给Pri 
+//								default:break;
+//							}
+//							switch(pri)				//根据球的颜色输出相应的文字 
+//							{
+//								case red:printf("%-10s","red");break;		//Pri的值等于枚举常量red时输出“red” 
+//								case yellow:printf("%-10s","yellow");break;	//Pri的值等于枚举常量yellow时输出“yellow“ 
+//								case blue:printf("%-10s","blue");break;		//Pri的值等于枚举常量blue时输出”blue“ 
+//								case white:printf("%-10s","white");break;	//Pri的值等于枚举常量white时输出”white“ 
+//								case black:printf("%-10s","black");break;	//Pri的值等于枚举常量black时输出“black” 
+//								default:break;
+//							}
+//						}
+//						printf("\n");
+//					}
+//				}
+//			}
+//		}
+//	}
+//	printf("一共有%d组符合题意。\n",n);
+//	return 0;
+//}
+//
+//#include <stdio.h> 
+//int main()
+//{
+//	enum color{red,yellow,blue,white,black};
+//	int i,j,k,pri;
+//	int n=0,loop;
+//	for(i=red;i<=black;i++)
+//	{
+//		for(j=red;j<=black;j++)
+//		{
+//			if(i!=j)
+//			{
+//				for(k=red;k<=black;k++)
+//				{
+//					if((i!=k) && (j!=k))
+//					{
+//						n++;
+//						printf("%4.2d",n);
+//						for(loop=1;loop<=3;loop++)
+//						{
+//							switch(loop)
+//							{
+//								case 1:pri=i;break;
+//								case 2:pri=j;break;
+//								case 3:pri=k;break;
+//								default:break;
+//							}
+//							switch(pri)
+//							{
+//								case red:printf("%10s","red");break;
+//								case yellow:printf("%10s","yellow");break;
+//								case blue:printf("%10s","blue");break;
+//								case white:printf("%10s","white");break;
+//								case black:printf("%10s","black");break;
+//							}
+//						}
+//						printf("\n"); 
+//					}
+//				}
+//			}
+//		}
+//	}
+//	printf("一共可能性有%5d种",n);
+//	return 0;
+//}
 
-int main()
-{
-	int i;
-	for(i=0;i<2;i++)
-	{
-		printf("please input the date of person:\n");
-		scanf("%d %s %c %c",&person[i].num,&person[i].name,&person[i].sex,&person[i].job );
-		if(person[i].job=='s')
-			scanf("%s",&person[i].category.clas);
-		else if(person[i].job=='t')
-			scanf("%s",&person[i].category.posision);
-		else
-			printf("input error!");
-	}
-	printf("\n");
-	printf("NO.	name	sex		job	class/posision\n");
-	for(i=0;i<2;i++)
-	{
-		if(person[i].job=='s')
-			printf("%-6d%-10d%-4c%-4c%-10d\n",person[i].num ,person[i].name ,person[i].sex ,person[i].job ,person[i].category.clas );
-		else
-			printf("%-6d%-10d%-4c%-4c%-10s\n",person[i].num ,person[i].name ,person[i].sex ,person[i].job ,person[i].category.posision);
-	}
-	return 0;
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
