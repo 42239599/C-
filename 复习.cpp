63362706ebe8cba0244f6e4b408e9a842176f280
42239599 <88647659+42239599@users.noreply.github.com>
2022-01-17 06:23:48 -0600

    Add files via upload

--- 复习.cpp
+++ 复习.cpp
@@ -1,158 +1,210 @@
 //#include <stdio.h> 
-//#include <math.h>
 //int main()
 //{
-//	double a,b,c,disc,x1,x2,realpart,imagpart;
-//	scanf("%lf %lf %lf",&a,&b,&c);
-//	printf("the equation");
-//	if(fabs(a)<=1e-6)			///判断是否是一元二次方程 
-//		printf(" is not a quadratic\n");	//不是 
-//	else
+//	int i=1,sum=0;
+//	while(i<=100)
 //	{
-//		disc=b*b-4*a*c;
-//		if(fabs(disc)<=1e-6)
-//			printf("has two equal roots:%8.4f\n",-b/(2*a));
-//		else if(disc>1e-6)
-//			{
-//				x1=(-b+sqrt(disc))/(2*a);
-//				x2=(-b-sqrt(disc))/(2*a);
-//				printf("has distinct real roots:%8.4f\t%8.4f\n",x1,x2);
-//			}
-//		else
+//		sum+=i;
+//		i++;
+//	}
+//	printf("结果是%d\n",sum);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int n=1,i=100,sum;
+//	sum=(i+n)*100/2;
+//	printf("结果是：%d\n",sum);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	float money,sum=0,aver;
+//	int count,i;
+//	for(i=0;i<1000;i++)
+//	{
+//		printf("请输入第%d位学生的捐款：",i+1);
+//		scanf("%f",&money);
+//		sum=sum+money;
+//		if(sum>=100000)
+//		break;
+//		count ++;
+//		printf("\n");
+//	}
+//	aver=sum/count;
+//	printf("捐款人数为：%d\n人均捐款为%5.2f\n",count,aver);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int n;
+//	for(n=100;n<=200;n++)
+//	{
+//		if(n%3!=0)
 //		{
-//			realpart=-b/(2*a);
-//			imagpart=sqrt(-disc)/(2*a);
-//			printf("%8.4f+%8.4fi\n",realpart,imagpart);
-//			printf("%8.4f-%8.4fi\n",realpart,imagpart);
+//			printf("%d\n",n);
 //		}
 //	}
 //	return 0;
 //}
 
+//#include <stdio.h>
+//int main()
+//{
+//	int n;
+//	for(n=100;n<=200;n++)
+//	{
+//		if(n%3==0)
+//			continue;
+//		printf("%d\n",n);
+//	}
+//	
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int i,j;
+//	for(i=1;i<=4;i++)
+//	{
+//		for(j=1;j<=5;j++)
+//		{
+//			printf("%4d",i*j);
+//			if(j%5==0)
+//			{
+//				printf("\n");
+//				break;	
+//			}
+//			
+//		}
+//	}
+//	
+//	return 0;
+//}
+
+//#include <math.h>
+//#include <stdio.h>
+//int main()
+//{
+//	float sum=0.0,fenzi=1.0,fenmu=1.0,terms=1.0;
+//	float result;
+//	while(fabs(terms)>=1e-6)
+//	{
+//		sum+=terms;	
+//		fenmu+=2;
+//		fenzi=-fenzi;
+//		terms=fenzi/fenmu;
+//	}
+//	result =sum*4;
+//	printf("result is:%5.2f\n",result);
+//	
+//	return 0;
+//}
+
+////斐波那契数列
 //#include <stdio.h> 
 //int main()
 //{
-//	double p,s,d,t,basic_p;
-//	printf("请输入单价： ");
-//	scanf("%lf",&basic_p);
-//	printf("\n\n\n请输入货物重量(单位:t)： ");
-//	scanf("%lf",&t);
-//	printf("\n\n\n请输入路程： ");
-//	scanf("%lf",&s);
-////	if(s<250)
-////		d=0;
-////	else if(s>=250 && s<500)
-////		d=0.02;
-////	else if(s>=500 && s<1000)
-////		d=0.05;
-////	else if(s>=1000 && s<2000)
-////		d=0.08;
-////	else if(s>=2000 && s<3000)
-////		d=0.10;
-////	else
-////		d=0.15; 
-//	int a;
-//	a=s/250;
-//	switch(a)
+//	int i,f1=1,f2=1;
+//	for(i=1;i<=20;i++)
 //	{
-//		case 12:d=0.15;break;
-//		case 11:
-//		case 10:
-//		case 9:
-//		case 8:d=0.1;break;
-//		case 7:
-//		case 6:
-//		case 5:
-//		case 4:d=0.08;break;
-//		case 3:
-//		case 2:d=0.05;break;
-//		case 1:d=0.02;break;
-//		case 0:d=0;break;
-//	}
-//	
-//	p=basic_p*t*s*(1-d);
-//	printf("总运费为：%5.3f\n\n",p);
-//	return 0;
-//}
-
-//#include <stdio.h>
-//int main()
-//{
-//	float score;
-//	printf("please input the score:\n");
-//	scanf("%f",&score);
-//	int a;
-//	a=score/10.0;
-//	printf("您的成绩等级是： ");
-//	switch(a)
-//	{
-//		case 10:
-//		case  9:printf("A\n");break;
-//		case 8:printf("B\n");break;
-//		case 7:printf("C\n");break;
-//		case 6:printf("D\n");break;
-//		case 5:
-//		case 4:
-//		case 3:
-//		case 2:	
-//		case 1:
-//		case 0:printf("E\n");break;
+//		printf("%12d%12d",f1,f2);
+//		if(i%2==0)
+//			printf("\n");
+//		f1=f1+f2;
+//		f2=f2+f1;
 //	}
 //	
 //	return 0;
 //}
 
+//求质数
+//#include <stdio.h> 
+//int main()
+//{
+//	int n,i;
+//	printf("请输入一个大于3的数：\n");
+//	scanf("%d",&n);
+//	for(i=2;i<=n-1;i++)
+//	{
+//		if(n%i==0)
+//		break;
+//	}
+//	if(i>=2&&i<=n-1)
+//		printf("不是质数\n");
+//	else
+//		printf("是质数\n");
+//	return 0;
+//}
+//中间数 
 //#include <stdio.h>
 //int main()
 //{
-//	int n,a,q,b,s,g;
-//	printf("请输入一个数：\n");
-//	scanf("%d",&a);
-//	if(a>=1000 && a<10000)
+//	int i,n,a;
+//	printf("请输入大于3的数：\n");
+//	scanf("%d",&n);
+//	a=n/2;
+//	for(i=2;i<=a;i++)
 //	{
-//		printf("这是一个四位数\n\n");
-//		q=a/1000;
-//		b=(a-q*1000)/100;
-//		s=(a-q*1000-b*100)/10;
-//		g=(a-q*1000-b*100-s*10);
-//		printf("各个数字分别是：\n千位：%d\n百位：%d\n十位：%d\n个位：%d\n",q,b,s,g);
-//		n=g*1000+s*100+b*10+q;
-//		printf("倒序输出为：\n%d\n",n);
+//		if(n%i==0)
+//		break;
 //	}
+//	if(i==a+1)
+//		printf("是素数\n");
+//	else
+//		printf("不是素数\n");
 //	return 0;
 //}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//开平方
+//#include <stdio.h>
+//#include <math.h> 
+//int main()
+//{
+//	int n,i,k;
+//	printf("please input a number:\n");
+//	scanf("%d",&n);
+//	k=sqrt(n);
+//	for(i=2;i<=k;i++)
+//	{
+//		if(n%i==0)
+//		break;
+//	}
+//	if(i>=k+1)
+//		printf("是素数\n");
+//	else
+//		printf("不是素数\n");
+//	return 0;
+//}
+//#include <math.h>
+//#include <stdio.h>
+//int main()
+//{
+//	int i,n,k,count;
+//	printf("素数如下：\n"); 
+//	for(n=100;n<=200;n++)
+//	{
+//		k=sqrt(n);
+//		for(i=2;i<=k;i++)
+//		{
+//			if(n%i==0)
+//				break;
+//		}
+//		if(i>=k+1)
+//		{
+//			printf("%d\n",n);	
+//			count ++;
+//		}
+//	}
+//	printf("一共有%d个\n",count);
+//	return 0;
+//}
 
 
 
