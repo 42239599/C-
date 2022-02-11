cc2501e443d2cc8af99c938845fff7c336ac9a76
42239599 <88647659+42239599@users.noreply.github.com>
2022-02-11 05:42:01 -0600

    Add files via upload

--- 复习.cpp
+++ 复习.cpp
@@ -1,387 +1,338 @@
 //#include <stdio.h> 
-//int main(void)
+//struct
 //{
-//	void sorts(int *q1,int *q2,int *q3);
-//	int a,b,c,*p1,*p2,*p3;
-//	printf("请输入3个数字：\n");
-//	scanf("%d%d%d",&a,&b,&c);
-//	p1=&a;p2=&b;p3=&c;
-//	sorts(p1,p2,p3);
-//	printf("排序如下：\n%d\t%d\t%d\n",*p1,*p2,*p3);
-//	return 0;
-//}
-//void sorts(int *q1,int *q2,int *q3)
-//{
-//	void sort(int *qt1,int *qt2);
-//	if(*q1>*q2)	sort(q1,q2);
-//	if(*q1>*q3)	sort(q1,q3);
-//	if(*q2>*q3)	sort(q2,q3);
-//}
-//void sort(int *qt1,int *qt2)
-//{
-//	int t;
-//	t=*qt1;
-//	*qt1=*qt2;
-//	*qt2=t;
-//}
-
-//#include <stdio.h>
-//#include <string.h>
-//int main(void)
-//{
-//	void sorts(char (*p),int n);
-//	char a[3][20];
-//	int i;
-//	for(i=0;i<3;i++)
+//	int num;
+//	char name [10];
+//	char sex;
+//	char job;
+//	union
 //	{
-//		printf("请输入%d个句子：\n",i+1);
-//		scanf("%s",a[i]);
-//	}
-//	
-//	sorts(a,3);
-//	
-//	printf("输出：\n");
-//	for(i=0;i<3;i++)
-//		printf("%s\n",a[i]);
-//	return 0;
-//}
-//void sorts(char (*p),int n)
-//{
-//	int i,j;
-//	char t[100];
-//	for(i=0;i<n-1;i++)
-//	{
-//		for(j=i+1;j<n;j++)
-//		{
-//			if(strcmp((p+i),(p+j))>0)	
-//			{
-//				strcpy(t,p+i);
-//				strcpy(p+i,p+j);
-//				strcpy(p+j,t);
-//			}	
-//			
-//		}
+//		int clas;
+//		char position[10];
+//	}category;
+//}person[2];
 //
-//	}
-//}
-
-//#include <stdio.h>
-//#include <string.h>
-//int main(void)
-//{
-//	void sort(char *q1,char *q2);
-//	char a[100],b[100],c[100],*p1,*p2,*p3;
-//	printf("请输入第一个句子：\n");
-//	gets(a);
-//	printf("请输入第二个句子：\n");
-//	gets(b); 
-//	printf("请输入第三个句子：\n");
-//	gets(c);
-//	p1=a;p2=b;p3=c;
-//	sort(p1,p2);
-//	sort(p1,p3);
-//	sort(p2,p3);
-//	printf("排序后：\n%s\n%s\n%s\n",a,b,c);
-//	return 0;
-//}
-//void sort(char *q1,char *q2)
-//{
-//	char t[100];
-//	if(strcmp(q1,q2)>0)
-//	{
-//		strcpy(t,q1);
-//		strcpy(q1,q2);
-//		strcpy(q2,t);
-//	}
-//}
-
-
-//#include <stdio.h>
-////#include <string.h>
-////int main(void)
-////{
-////	void sort(char a[][100]);
-////	char a[3][100];
-////	int i;
-////	for(i=0;i<3;i++)
-////	{
-////		printf("请输入第%d个句子：\n",i+1);
-////		scanf("%s",a[i]);
-////	}
-////	sort(a[]);
-////	printf("输出如下：\n");
-////	for(i=0;i<3;i++)
-////	{
-////		printf("%s\n",a[i]);
-////	}
-////	return 0;
-////}
-////void sort(char a[])
-////{
-////	int i;	
-////}
-//
-//#include <stdio.h>
-//int main(void)
-//{
-//	void enter(int *q,int n);
-//	void opa(int *q,int n);
-//	void print(int *q,int n);
-//	int a[10],*p=a;
-//	enter(p,10);
-//	opa(p,10);
-//	print(p,10);
-//	return 0;
-//}
-//void enter(int *q,int n)
+//int main()
 //{
 //	int i;
-//	printf("请输入十个数：\n");
-//	for(i=0;i<n;i++)
+//	for(i=0;i<2;i++)
 //	{
-//		scanf("%d",(q+i));
+//		printf("请输入第%d位的信息：\n",i+1);
+//		scanf("%d %s %c %c",&person[i].num ,&person[i].name ,&person[i].sex ,&person[i].job );
+//		if(person[i].job == 's')
+//			scanf("%d",&person[i].category.clas );
+//		else if(person[i].job == 't')
+//			scanf("%s",person[i].category.position );
+//		else
+//			printf("输入有误！！\n");
 //	}
-//	printf("原数组排序为：\n");
-//	for(i=0;i<n;i++)
-//	{
-//		printf("%d\t",*(q+i));
-//	}
-//}
-//void opa(int *q,int n)
-//{
-//	int i,j,maxi,mini,fir_i=0,las_i=n-1,t1,t2;
-////	int max=*q;
-////	for(i=0;i<n;i++)
-////	{
-////		if(max<=*(q+i))
-////		{
-////			max=*(q+i);
-////			maxi=i;
-////		}
-////	}
-////	int min=*q;	
-////	for(i=0;i<n;i++)
-////	{
-////		if(min>=*(q+i))
-////		{
-////			min=*(q+i);
-////			mini=i;
-////		}
-////	}
 //	
-//	int max=*q;
-//	int min=*q;
-//	for(i=0;i<n;i++)
-//	{
-//		if(*(q+i)>=max)
-//		{
-//			max=*(q+i);
-//			maxi=i;
-//		}
-//		
-//		if(*(q+i)<=min)
-//		{
-//			min=*(q+i);
-//			mini=i;
-//		}
-//		
-//	}
-//	t1=*(q+fir_i);*(q+fir_i)=*(q+mini);*(q+mini)=t1;
-//	t2=*(q+las_i);*(q+las_i)=*(q+maxi);*(q+maxi)=t2;
+//	printf("\n\n");
 //	
-//}
-//void print(int *q,int n)
-//{
-//	int i;
-//	printf("\n输出如下：\n");
-//	for(i=0;i<n;i++)
-//		printf("%d\t",*(q+i));
+//	printf("num\tname\tsex\tjob\tclas/position\n");
+//	for(i=0;i<2;i++)
+//	{
+//		if(person[i].job == 's')
+//			printf("%d\t%s\t%c\t%c\t%d\n\n",person[i].num ,person[i].name ,person[i].sex ,person[i].job ,person[i].category.clas );
+//		else
+//			printf("%d\t%s\t%c\t%c\t%s\n\n",person[i].num ,person[i].name ,person[i].sex ,person[i].job ,person[i].category.position );
+//	}
+//	return 0;
 //}
 
 
-////18、
-////输入10个整数,将其中最小的数与第一个数对换,把最大的数与最后一个对换，写三个函数：①输入10个数；②进行处理；③输出10个数。
 //#include <stdio.h>
 //int main()
 //{
-//	void enter(int *q,int n);//输入函数 
-//	void swap(int *q,int n);//处理函数 
-//	void print(int *q,int n);//打印函数 
-//	int a[10],*p=a;
-//	enter(p,10);
-//	swap(p,10);
-//	print(p,10);
-//	return 0;
-//}
-////输入函数 
-//void enter(int *q,int n)
-//{
-//	int i;
-//	printf("请输入十个数：\n");
-//	for(i=0;i<10;i++)
+//	enum color{red,yellow,blue,white,black
+//	};
+//	int i,j,k,pri;
+//	int n,loop;
+//	n=0;
+//	for(i=red;i<=black;i++)
 //	{
-//		scanf("%d",&*(q+i));
-//	}
-//}
-////处理函数 
-//void swap(int *q,int n)
-//{
-//	int max,min,i,t1,t2,MIN,MAX;
-//	max=*q;
-//	//找出最大值的位置 
-//	for(i=0;i<n;i++)
-//	{
-//		if(*(q+i)>=max)
+//		for(j=red;j<=black;j++)
 //		{
-//			max=*(q+i);
-//			MAX=i;
-//		}	
-//	}
-//	//找出最小值的位置
-//	min=*q;	 
-//	for(i=0;i<n;i++)
-//	{
-//		if(*(q+i)<=min)
-//		{
-//			min=*(q+i);
-//			MIN=i;
+//			if(i!=j)
+//			{
+//				for(k=red;k<=black;k++)
+//				{
+//					if((i!=k) &&  (j!=k))
+//					{
+//						n=n+1;
+//						printf("%-4d",n);
+//						for(loop=1;loop<=3;loop++)
+//						{
+//							switch(loop)
+//							{
+//								case 1:pri=i;break;
+//								case 2:pri=j;break;
+//								case 3:pri=k;break;
+//								default:break;
+//							}
+//							switch(pri)
+//							{
+//								case red:printf("%-10s","red");break;
+//								case yellow:printf("%-10s","yellow");break;
+//								case blue:printf("%-10s","blue");break;
+//								case white:printf("%-10s","white");break;
+//								case black:printf("%-10s","black");break;
+//								default:break;
+//							}
+//						}
+//						printf("\n");
+//					}
+//				}
+//			}
 //		}
 //	}
-//	//交换 
-//	t1=*(q+MAX);*(q+MAX)=*(q+n-1);*(q+n-1)=t1;//大值交换位置 
-//	t2=*(q+MIN);*(q+MIN)=*q;*q=t2;			  //小值交换位置 
+//	printf("总共有%d种情况\n\n",n);
+//	return 0;
 //}
-////打印函数 
-//void print(int *q,int n)
+
+
+//#include <stdio.h>
+//struct Date
 //{
-//	int i;
-//	printf("处理后的结果是：\n");
-//	for(i=0;i<10;i++)
+//	int year;
+//	int month;
+//	int day;
+//}date;
+//int main()
+//{
+//	int sum=0;
+//	printf("请输入要计算的年月日：\n");
+//	scanf("%d %d %d",&date.year ,&date.month ,&date.day );
+//	switch(date.month )
 //	{
-//		printf("%d ",*(q+i));
+//		case 12:sum+=31;
+//		case 11:sum+=30;
+//		case 10:sum+=31;
+//		case 9:sum+=30;
+//		case 8:sum+=31;
+//		case 7:sum+=31;
+//		case 6:sum+=30;
+//		case 5:sum+=31;
+//		case 4:sum+=30;
+//		case 3:sum+=31;
+//		case 2:sum+=28;
+//		case 1:sum+=date.day ;
 //	}
+//	
+//	if((date.year %4==0 &&date.year %100 != 0) || date.year % 400 == 0)
+//	{
+//		if(date.month >=3)
+//		sum+=1;
+//	}
+//	
+//	printf("结果是第%d天\n\n",sum);
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//struct Year
+//{
+//	int year;
+//	int month;
+//	int day;
+//}a;
+//int main()
+//{
+//	int days(int *q1,int *q2,int *q3);
+//	int *p1,*p2,*p3,c;
+//	p1=&a.year;
+//	p2=&a.month ;
+//	p3=&a.day ;
+//	printf("请输入年月日：\n");
+//	scanf("%d %d %d",p1,p2,p3);
+//	c=days(p1,p2,p3);
+//	printf("输入的日期是该年的第%d天。\n",c);
+//	return 0;
+//}
+//int days(int *q1,int *q2,int *q3)
+//{
+//	int sum=0;
+//	switch(*q2)
+//	{
+//		case 12:sum+=31;
+//		case 11:sum+=30;
+//		case 10:sum+=31;
+//		case 9:sum+=30;
+//		case 8:sum+=31;
+//		case 7:sum+=31;
+//		case 6:sum+=30;
+//		case 5:sum+=31;
+//		case 4:sum+=30;
+//		case 3:sum+=31;
+//		case 2:sum+=28;
+//		case 1:sum+=*q3;
+//	}
+//	if(*q1%4==0 && *q1%100 != 0 ||*q1%400 == 0)
+//	{
+//		if(*q2>=3)
+//		sum+=1;	
+//	}
+//	return sum;
+//}
+
+
+// 定义一个结构体变量(包括年、月、日)。计算该日在本年中是第几天，注意闰年问题。
+//写一个函数days,实现第1题的计算。由主函数将年、月、日传递给days函数.
+//计算后将日子数传回主函数输出。
+//#include<stdio.h>
+//
+//int days(int year,int month,int day)
+//{
+//	int day_sum=0;
+//	int month_sum[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};// 12个月每月的天数 
+//	for(int i=0;i<month;i++)
+//	{
+//		day_sum+=month_sum[i];//month个月
+//	}
+//	
+//	day_sum=day_sum+day;//加上这个月的day 
+//	
+//	if ((year%4==0 && year%100!=0 || year%400==0) && month > 2)//判断闰年 ，1月和2月不用考虑 
+//	{
+//		day_sum+=1;//闰年2月多一天 
+//	}	
+//	return day_sum; 
 //}
 //
-//#include <stdio.h>
-//int main(void)
+//
+//struct date 
 //{
-//	void enter(int a[],int n);
-//	void sorts(int a[],int n);
-//	void print(int a[],int n);
+//	int year;
+//	int month;
+//	int day;
+//};
+//int main()
+//{
+//	struct date yanlu; 
+//	int sum;
+//	printf("请输入年月(1-12)日(1-31)：");
+//	scanf("%d %d %d",&yanlu.year,&yanlu.month,&yanlu.day);
+//	sum=days(yanlu.year,yanlu.month,yanlu.day);//调用time返回总天数 
+//	printf("\n%d月%d日是%d年的第%d天。",yanlu.month,yanlu.day,yanlu.year,sum);
 //	
-//	int a[10];
-//	enter(a,10);
-//	sorts(a,10);
-//	print(a,10);
 //	return 0;
-//}
-//void enter(int a[],int n)
-//{
-//	int i;
-//	printf("请输入十个数字：\n");
-//	for(i=0;i<10;i++)
-//	{
-//		scanf("%d",&a[i]);
-//	}
-//	
-//	printf("原数组为：\n");
-//	for(i=0;i<n;i++)
-//		printf("%d\t",a[i]);
-//}
-//void sorts(int a[],int n)
-//{
-//	int last=n-1,first=0,i,t1,t2,maxi,mini;
-//	int max=a[0];
-//	int min=a[0];
-//	for(i=0;i<n;i++)
-//	{
-//		if(max<=a[i])
-//		{
-//			max=a[i];
-//			maxi=i;
-//		}
-//		if(min>=a[i])
-//		{
-//			min=a[i];
-//			mini=i;
-//		}
-//	}
-//	
-//	t1=a[first];a[first]=a[mini];a[mini]=t1;
-//	t2=a[last];a[last]=a[maxi];a[maxi]=t2;
-//	
-//}
-//void print(int a[],int n)
-//{
-//	int i;
-//	printf("\n整理后：\n");
-//	for(i=0;i<n;i++)
-//	{
-//		printf("%d\t",a[i]);
-//	}
 //}
 
 //#include <stdio.h>
-//int main(void)
+//struct Date
 //{
-//	void sorts(int a[],int n,int m);
-//	int a[100],i,n,m;
-//	printf("请输入的数字个数：\n");
-//	scanf("%d",&n);
-//	printf("请输入每个数字：\n");
-//	for(i=0;i<n;i++)
+//	int year;
+//	int month;
+//	int day;
+//}date;
+//
+//int main()
+//{
+//	int i,sum_day=0;
+//	int mon_day[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};//12个月每天的天数 
+//	printf("请输入要计算的年月日\n");
+//	scanf("%d %d %d",&date.year ,&date.month ,&date.day );
+//	for(i=0;i<date.month ;i++)
 //	{
-//		scanf("%d",&a[i]);
+//		sum_day+=mon_day[i] ;//前几个月相加 
 //	}
-//	printf("请输入要后移的位置：\n");
-//	scanf("%d",&m);
-//	sorts(a,n,m);
-//	printf("整理后的结果如下：\n");
-//	for(i=m;i<m+n;i++)
+//	
+//	sum_day+=date.day;	//加上当月的天数 
+//	
+//	if((date.year %4 == 0 && date.year %100 != 0 || date.year % 400 == 0) && (date.month > 2))//判断闰年，1、2月不用考虑 
 //	{
-//		printf("%d\t",a[i]);
+//		sum_day+=1; 	//闰年加一天 
 //	}
+//	
+//	printf("%d月%d日是%d年的第%d天。\n\n",date.month ,date.day ,date.year ,sum_day);
+//	
 //	return 0;
-//}
-//void sorts(int a[],int n,int m)
-//{
-//	int i;
-//	for(i=0;i<m;i++,n++)
-//	{
-//		a[n]=a[i];
-//	}
 //}
 
-//#include <stdio.h>
-//int main(void)
+
+//#include <stdio.h> 
+//struct Date
 //{
-//	void sort(int a[],int n,int m);
-//	int a[100],n,m,i;
-//	printf("请输入数字个数：\n");
-//		scanf("%d",&n);
-//	printf("请输入各个数字：\n");
-//	for(i=0;i<n;i++)
-//		scanf("%d",&a[i]);
-//	printf("请输入要后移的位数：\n");
-//		scanf("%d",&m);
-//	
-//	sort(a,n,m);
-//	
-//	printf("\n输出的结果是：\n");
-//	for(i=n-m;i<m+n;i++)
-//	{
-//		printf("%d\t",a[i]);
-//	}
+//	int year;
+//	int month;
+//	int day;
+//}date;
+//int main()
+//{
+//	int add(int year,int month,int day);
+//	int sum;
+//	printf("请输入年月日：\n");
+//	scanf("%d %d %d",&date.year ,&date.month ,&date.day );
+//	sum=add(date.year ,date.month ,date.day );
+//	printf("%d月%d日是%d年的第%d天\n\n",date.month ,date.day ,date.year ,sum);
 //	return 0;
 //}
-//void sort(int a[],int n,int m)
+//int add(int year,int month,int day)
 //{
-//	int i;
-//	for(i=0;i<n-m;i++,n++)
+//	int i,sum_day=0;
+//	int month_day[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};
+//	for(i=0;i<month;i++)
 //	{
-//		a[n]=a[i];
+//		sum_day+=month_day[i];
 //	}
+//	
+//	sum_day+=day;
+//	
+//	if((month % 4 == 0 && month % 100 != 0 || month %400 == 0) && (month > 2))
+//	{
+//		sum_day++;
+//	}
+//	return sum_day;
 //}
+
+
+#include <stdio.h>
+struct student
+{
+	int num;
+	char name[10];
+	float score[3];
+	float aver;
+}stu[10];
+int main()
+{
+	void input(struct student stu[]);
+
+	input(stu);
+
+	
+	return 0;
+}
+void input(struct student stu[])
+{
+	int i,j;
+	float aver[3],sum[3];
+	for(i=0;i<10;i++)
+	{
+		printf("请输入第%d位学生的信息：\n",i+1);
+		scanf("%d %s %f %f %f",&stu[i].num,stu[i].name,&stu[i].score[0],&stu[i].score[1],&stu[i].score[2]);
+	}
+	for(j=0;j<3;j++)
+	{
+		for(i=0;i<10;i++)
+		{
+			sum[j]=stu[i].score[0];
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
